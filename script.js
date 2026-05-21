4fd212afaa0180c6610e215055d1e48b898af82b
Vichhay <75215104+vichhay97@users.noreply.github.com>
2026-05-21 14:44:08 +0200

    Add playRound function that handles play logic

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let computerScore = 0;
+let humanScore = 0;
+
 function getComputerChoice() {
     const val = Math.floor(Math.random() * 3) + 1;
 
@@ -17,3 +20,49 @@ function getHumanChoice() {
     return userInput.trim().toLowerCase();
 }
 
+function playRound(computerChoice, humanChoice) {
+    switch (computerChoice) {
+        case "rock":
+            if (humanChoice === "paper") {
+                console.log("You win! Paper beats Rock.")
+                humanScore++;
+            }
+            else if (humanChoice === "scissors")
+            {
+                console.log("You lose! Rock beats Scissors.")
+                computerScore++;
+            }
+            else {
+                console.log("That's a tie! You both picked rock.")
+            }
+            break;
+        case "paper":
+            if (humanChoice === "rock") {
+                console.log("You lose! Paper beats Rock.")
+                computerScore++;
+            }
+            else if (humanChoice === "scissors")
+            {
+                console.log("You win! Scissors beat Paper.")
+                humanScore++;
+            }
+            else {
+                console.log("That's a tie! You both picked paper.")
+            }
+            break;
+        case "scissors":
+            if (humanChoice === "rock") {
+                console.log("You win! Rock beats Scissors.")
+                humanScore++;
+            }
+            else if (humanChoice === "paper")
+            {
+                console.log("You lose! Scissors beat Paper.")
+                computerScore++;
+            }
+            else {
+                console.log("That's a tie! You both picked scissors.")
+            }
+            break;
+    }
+}
